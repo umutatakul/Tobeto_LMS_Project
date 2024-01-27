8aff53c39440a4e3d62ce613a31edfe5ae2f2812
Umut Atakul <atakulumut@gmail.com>
2024-01-27 17:45:25 +0300

    Gereksiz import dosyaları temizlendi

--- tobeto_lms_project/lib/models/exam_result.dart
+++ tobeto_lms_project/lib/models/exam_result.dart
@@ -1,5 +1,3 @@
-import 'package:tobeto_lms_project/models/course.dart';
-
 class ExamResult {
   ExamResult({required this.name, required this.date, required this.score});
   final String name;
--- tobeto_lms_project/lib/widgets/profile_widgets/level_test_result_card.dart
+++ tobeto_lms_project/lib/widgets/profile_widgets/level_test_result_card.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:tobeto_lms_project/models/exam_result.dart';
