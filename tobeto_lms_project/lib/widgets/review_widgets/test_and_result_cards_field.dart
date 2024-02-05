df94139d5f83918f666d71f61499c13061b501cc
Umut Atakul <atakulumut@gmail.com>
2024-02-05 18:02:50 +0300

    review sayfası içindeki "test and result" kısmı için önceden oluşturduğum kontrol kısımlarını fonksiyon haline getirildi

--- tobeto_lms_project/lib/widgets/review_widgets/test_and_result_cards_field.dart
+++ tobeto_lms_project/lib/widgets/review_widgets/test_and_result_cards_field.dart
@@ -13,25 +13,58 @@ class TestAndResultCardsField extends StatelessWidget {
       children: [
         for (String examName in requiredExmamsList)
           ExamCard(
-            examName: examName,
-            scorePercent: resultListScores[resultListScores.indexWhere(
-                            (element) => element.name == examName) !=
-                        -1
-                    //eğer index yoksa -1 (eksi bir döndürüyor)
-                    ? resultListScores
-                        .indexWhere((element) => element.name == examName)
-                    : 0]
-                .score,
-            scorePercentVisibility: resultListScores
-                .where((element) => element.name == examName)
-                .toList()
-                .isNotEmpty,
-            isScoreNotEmpty: resultListScores
-                .where((element) => element.name == examName)
-                .toList()
-                .isNotEmpty,
-          )
+              examName: examName,
+              scorePercent: scorePercentController(resultListScores, examName),
+              //  resultListScores[resultListScores.indexWhere(
+              //                 (element) => element.name == examName) !=
+              //             -1
+              //         //eğer index yoksa -1 (eksi bir döndürüyor)
+              //         ? resultListScores
+              //             .indexWhere((element) => element.name == examName)
+              //         : 0]
+              //     .score,
+              scorePercentVisibility:
+                  scorePercentVisibilityController(resultListScores, examName),
+              //  resultListScores
+              //     .where((element) => element.name == examName)
+              //     .toList()
+              //     .isNotEmpty,
+              isScoreNotEmpty: isScoreThere(resultListScores, examName)
+              // resultListScores
+              //     .where((element) => element.name == examName)
+              //     .toList()
+              //     .isNotEmpty,
+              )
       ],
     );
   }
+}
+//TODO Controllerları ayrı ayrı dosya olarak closöre taşı
+
+//Mock datamdaki sınav sonuçlarını, ekranda zorunlu olarak listelemek istediğim
+//sınav adları ile  kıyasla. Eğer varsa listedeki indekisini al ve ordaki scoru döndür
+//eğer -1 yani index yoksa 0 ı döndür
+//TODO Veriler için type safe olması amacı ile veri tiplerini tanımla
+double scorePercentController(List listOfResults, String nameOfExam) {
+  final int indexOfCurrentScore =
+      listOfResults.indexWhere((element) => element.name == nameOfExam);
+  return listOfResults[indexOfCurrentScore != -1
+          //eğer index yoksa -1 (eksi bir ) döndürüyor
+          ? indexOfCurrentScore
+          : 0]
+      .score;
+}
+
+bool scorePercentVisibilityController(
+    List listOfResultScores, String nameOfExam) {
+  return isScoreThere(listOfResultScores, nameOfExam);
+}
+
+//for içindeki mevcut irdelenen examName i mock datamdaki score listemle kıyasla
+//mevcutsa listeme ekle liste boş mu diye bak
+bool isScoreThere(List listOfScores, String nameOfExam) {
+  return listOfScores
+      .where((element) => element.name == nameOfExam)
+      .toList()
+      .isNotEmpty;
 }
