160793648853884324e9372d43365cf424684104
Umut Atakul <atakulumut@gmail.com>
2024-03-03 19:31:05 +0300

    Aktivite haritasında (dashboard) geçici bilgilerle denendi

--- tobeto_lms_project/lib/widgets/profile_widgets/activity_chart.dart
+++ tobeto_lms_project/lib/widgets/profile_widgets/activity_chart.dart
@@ -8,13 +8,24 @@ class ActivityChart extends StatelessWidget {
 //Burdan yapıldı.
   @override
   Widget build(BuildContext context) {
+    // İşleme yapmaya göre durumu kaydedip göstermi dene
     return HeatMap(
       datasets: {
-        DateTime(2024, 1, 27): 3,
-        DateTime(2024, 1, 28): 7,
-        DateTime(2024, 1, 29): 10,
-        DateTime(2024, 1, 30): 13,
-        DateTime(2024, 1, 31): 6,
+        DateTime(2024, 3, 1): 3,
+        DateTime(2024, 3, 2): 7,
+        DateTime(2024, 3, 4): 10,
+        DateTime(2024, 3, 5): 13,
+        DateTime(2024, 3, 7): 6,
+        DateTime(2024, 3, 10): 3,
+        DateTime(2024, 3, 12): 7,
+        DateTime(2024, 3, 14): 10,
+        DateTime(2024, 3, 15): 13,
+        DateTime(2024, 3, 17): 6,
+        DateTime(2024, 3, 20): 3,
+        DateTime(2024, 3, 22): 7,
+        DateTime(2024, 3, 24): 10,
+        DateTime(2024, 3, 25): 13,
+        DateTime(2024, 3, 27): 6,
       },
       startDate: DateTime.now(),
       endDate: DateTime.now().add(const Duration(days: 30)),
